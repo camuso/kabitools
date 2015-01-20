0fbffc6c086b81bcffeab33c1dd62e27f18af54d
Tony Camuso <tcamuso@redhat.com>
2015-01-20 12:47:46 -0500

    makei.sh: updated for speed.
    Instead of using find to invoke make one file at a time, create a
    variable containing all the filenames to be preprocessed and pass
    that as a command line argument to make. This provides a tremendous
    improvement in speed.
    
    Added some info for the user.

--- makei.sh
+++ makei.sh
@@ -30,7 +30,48 @@ if ! [ -d "$dirspec" ]; then
 	usage
 fi
 
-set -x
+cpucount=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+# leave one processor on multi-processor systems.
+#
+[ cpucount -gt 1 ] && cpucount=((cpucount - 1))
+
+echo -e "\n********* Creating file list *********** \n"
+echo -e "\tThis could take a while.\n"
+
+files=$(find $dirspec -type f -name \*.c -exec sh -c \
+	'path=$(dirname $1); \
+	file=$(basename $1); \
+	suffix=${file#*.}; \
+	[ "$suffix" != "c" ] && exit; \
+	stem=${file%.*}; \
+	echo -n "$path/$stem.i "' \
+	sh '{}' \;)
+
+echo -e "\n************** make the .i files *****************\n"
+echo -e "\tGo get some coffee. This will take some time."
+echo -e "\tDon't be alarmed by errors. They are expected."
+echo -e "\tThis script seeks to execute the gcc preprocessor"
+echo -e "\ton every .c file in the tree starting at $dirspec."
+echo -e "\tHowever, some files are not part of the kernel build"
+echo -e "\tas defined by the .config contents.\n"
+echo -e "\t10 second countdown before start.\n"
+echo
+echo "make -k -j$cpucount \<list of .i files\>"
+echo
+for ((i=0; i<10; ++i)); do
+	echo -n "."
+	sleep 1
+done
+
+echo ""
+
+make -k -j$cpucount $files 2>/dev/null
+
+exit 0
+
+# Deprecated code that built the .i files one at a time.
+#
 find $dirspec -name \*.c -exec sh -c \
 	'path=$(dirname $1); \
 	file=$(basename $1); \
