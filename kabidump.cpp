68938f9279f7a26d583fef62b20876eb05ea0a68
Tony Camuso <tcamuso@redhat.com>
2015-03-26 16:23:02 -0400

    kabidump: add headings with licensing info

--- kabidump.cpp
+++ kabidump.cpp
@@ -1,3 +1,29 @@
+/* kabidump.cpp - class to dump .dat file created by kabi-parser
+ *
+ * Copyright (C) 2015  Red Hat Inc.
+ * Tony Camuso <tcamuso@redhat.com>
+ *
+ * Boost serialization used by kabi-parser does not leave legible text files,
+ * even though it is a text archive. There is no '\n' after each record.
+ * This utility makes the data legible, and dumps the output to stdout.
+ * Output can be redirected to a file by the user.
+ *
+ * This is free software. You can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
 #include <boost/lexical_cast.hpp>
 #include "kabidump.h"
 
