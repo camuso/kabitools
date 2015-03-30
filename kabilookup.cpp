d4e942bc8ba67f855f8effccbaf06bacc4452d06
Tony Camuso <tcamuso@redhat.com>
2015-03-30 07:09:43 -0400

    kabilookup.cpp: removed the spinner
    Not necessary, since we are showing the files as we go.

--- kabilookup.cpp
+++ kabilookup.cpp
@@ -292,21 +292,6 @@ int lookup::get_children_wide(qnode& qn)
 	return EXE_OK;
 }
 
-void lookup::show_spinner(int& count)
-{
-	if (count % 0x10000)
-		cout << "|\r";
-	if (count % 0x14000)
-		cout << "\\\r";
-	if (count % 0x18000)
-		cout << "-\r";
-	if (count % 0x1c000)
-		cout << "/\r";
-
-	cout.flush();
-	++count;
-}
-
 int lookup::exe_exports()
 {
 	int count = 0;
@@ -342,7 +327,6 @@ int lookup::exe_exports()
 		for (auto it : m_qnodes) {
 			qnode& qn = it.second;
 			qn.crc = it.first;
-			show_spinner(count);
 
 			if (!(qn.flags & CTL_EXPORTED))
 				continue;
@@ -379,7 +363,6 @@ int lookup::exe_decl()
 		qnit = find_if (range.first, range.second,
 			  [this, &count](qnpair_t& lqp)
 			  {
-				show_spinner(count);
 				qnode& rqn = lqp.second;
 				return ((rqn.children.size() > 0) &&
 					(rqn.flags & CTL_HASLIST) &&
@@ -400,7 +383,6 @@ int lookup::exe_decl()
 		for (auto it : m_qnodes) {
 			qnode& rqn = it.second;
 			rqn.crc = it.first;
-			show_spinner(count);
 
 			if ((rqn.sdecl.find(m_declstr) != string::npos) &&
 			    (rqn.children.size() > 0) &&
