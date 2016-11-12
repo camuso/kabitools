4237b7c4643916ef45bd88e84bdd21bbe318de9a
Tony Camuso <tcamuso@redhat.com>
2016-11-12 14:37:47 -0500

    kabitools.sh: improvements and fixes
    Added an option to 'make clean' rather than making it the default.
    Added a trap for control-c, so we could reverse the Makefile patch
    before exiting.
    Added ability to determine the distro in order to select the correct
    kernel Makefile patch.
    Added strings for error exits.

--- kabitools.sh
+++ kabitools.sh
@@ -12,6 +12,7 @@ kabitools.sh -[j | h]
 
 	Must be invoked from the top of the kernel tree.
 
+	-c	- make clean first
 	-j	- Optional number of processors to assign to the make task.
 	-h	- This help screen.
 
@@ -29,16 +30,76 @@ kabitools.sh -[j | h]
 EOF
 )
 
+no_patch_str=$(
+cat <<EOF
+
+$(basename $0) could not find the correct patch for the kernel Makefiles.
+
+Please check for the existence of:
+
+/usr/share/kabitools-rhel-kernel-make.patch
+/usr/share/kabitools-fedora-kernel-make.patch
+
+\0
+EOF
+)
+
+no_os_str=$(
+cat <<EOF
+
+$(basename $0) does not handle distro: $distro
+
+\0
+EOF
+)
+
+not_kernel_str=$(
+cat <<EOF
+
+$(basename $0) must be invoked from the top of a kernel tree.
+
+You are here: $PWD
+
+\0
+EOF
+)
+
 usage() {
 	echo -e "$usagestr"
 	exit
 }
 
-declare cpus
+# run if user hits control-c
+#
+control_c()
+{
+	echo -en "\nCtrl-c detected\nCleaning up and exiting.\n"
+	patch -Rs -p1 < "$patch"
+  	exit 127
+}
 
-while getopts hj: OPTION; do
+[[ -f README ]] || { echo -e "$not_kernel_str"; exit 3; }
+
+declare kerneltree="$(head -1 README)"
+kerneltree=$(echo $kerneltree)
+[[ ${kerneltree:0:20} == "Linux kernel release" ]] || \
+	{ echo -e "not_kernel_str"; exit 4; }
+
+
+declare cpus
+declare clean=false
+declare patch
+declare distro="$(cat /etc/os-release | grep -w ID | cut -d'=' -f2)"
+
+# strip outside quotes from the distro string
+#
+distro=$(sed -e 's/^"//' -e 's/"$//' <<<"$distro")
+
+while getopts chj: OPTION; do
     case "$OPTION" in
 
+	c )	clean=true
+		;;
 	h ) 	usage
 		;;
 	j )	cpus=$OPTARG
@@ -49,6 +110,24 @@ while getopts hj: OPTION; do
     esac
 done
 
-patch -p1 < /usr/share/kabitools-rhel-kernel-make.patch
+case $distro in
+	"rhel")		patch="/usr/share/kabitools-rhel-kernel-make.patch"
+			;;
+	"fedora")	patch="/usr/share/kabitools-fedora-kernel-make.patch"
+			;;
+	* )		echo -e "$no_patch_str"
+			exit 1
+			;;
+esac
+
+[[ -e "$patch" ]] || { echo -e "$no_os_str"; exit 2; }
+
+patch -s -p1 < "$patch"
+
+# trap keyboard interrupt (control-c)
+trap control_c SIGINT
+
+$clean && make clean
 make -j $cpus K=1
-patch -R -p1 < /usr/share/kabitools-rhel-kernel-make.patch
+patch -Rs -p1 < "$patch"
+exit 0
