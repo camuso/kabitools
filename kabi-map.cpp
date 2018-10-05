ccfc4c9fe12c5ee72104da1cf895169fc3ccfb47
Tony Camuso <tcamuso@redhat.com>
2018-10-05 14:34:58 -0400

    kabi-map: must explicitly include iostream

--- kabi-map.cpp
+++ kabi-map.cpp
@@ -22,6 +22,7 @@
 #include <map>
 #include <cstring>
 #include <fstream>
+#include <iostream>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/serialization/map.hpp>
